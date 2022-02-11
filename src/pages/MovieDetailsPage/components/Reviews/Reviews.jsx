a8ba9194693f1eebacc9d864598906124d716edd
Alexandr Kondratenko <alexandr.kondratenko.exp@gmail.com>
2022-02-11 07:24:41 +0200

    first commit

--- src/pages/MovieDetailsPage/components/Reviews/Reviews.jsx
+++ src/pages/MovieDetailsPage/components/Reviews/Reviews.jsx
@@ -3,5 +3,6 @@ import { MovieDetailsContext } from 'context/MovieDetailsContext';
 
 export const Reviews = () => {
   const { reviews } = useContext(MovieDetailsContext);
+  console.log(reviews);
   return <div>Reviews</div>;
 };
